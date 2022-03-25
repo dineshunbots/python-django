c3623ac8cf9ef7744d010038642006237b519f97
dineshkumar <dineshkumar.unbots@gmail.com>
2022-03-25 15:36:45 +0530

    changes

--- upzet/urls.py
+++ upzet/urls.py
@@ -23,6 +23,7 @@ from django.contrib.auth.decorators import login_required
 
 from django.views.static import serve
 from django.conf.urls import url
+from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
@@ -53,4 +54,4 @@ urlpatterns = [
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
-#urlpatterns = urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns = urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
