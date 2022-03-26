5a0a94cd40deb2afc56e64e4f0c2464763029aab
dineshkumar <dineshkumar.unbots@gmail.com>
2022-03-26 11:16:38 +0530

    t

--- upzet/settings.py
+++ upzet/settings.py
@@ -22,8 +22,8 @@ MESSAGE_TAGS = {
 }
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-#BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -32,9 +32,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-8s))v3p!c1+wxk=l&3y0a#45g%i%m9s1u@k806pz1=p#r=yz)i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','test-pytho-django.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
